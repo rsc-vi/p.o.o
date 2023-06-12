ff39393e2057a3857ec21d0e11d94c2900a04751
JaciBarbosa <131264174+JaciBarbosa@users.noreply.github.com>
2023-06-12 08:43:59 -0300

    Update classes.js

--- Passageiros/classes.js
+++ Passageiros/classes.js
@@ -1,48 +1,53 @@
 // 1° 
 class Passageiro {
-    constructor(andarPretendido) {
+    constructor(andarPretendido, peso) {
         this.andarPretendido = andarPretendido;
+        this.peso = peso;
     };
+
 };
 // 2°
 class Elevador {
-    constructor() {
+    constructor(pesoMaximoSuportado) {
         this.andarAtual = 0
-        this.quantidadeMaximaDePassageiros = 9
+        this.pesoMaximoSuportado = 9
         this.listaDePassageiros = []
 
     };
     // 3°
-    adicionarPassageiro(passageiro) {
+    pesoMaximoSuportado(passageiro) {
 
-        if (this.listaDePassageiros.length < this.quantidadeMaximaDePassageiros) {
+        if (this.listaDePassageiros.length < this.pesoMaximoSuportado) {
             this.listaDePassageiros.push(passageiro);
 
-            console.log(`1 passageiro que deseja ir ao andar ${passageiro.andarAtual} entrou no elevador. Agora o elevador tem ${this.listaDePassageiros.length} passageiros.`);
+            console.log(`1 passageiro que deseja ir ao andar ${passageiro.andarPretendido} entrou no elevador. Agora o elevador tem ${this.listaDePassageiros.length} passageiros.`);
         } else {
-            (`O passageiro não pode entrar no elevador. Agora o elevador tem ${this.listaDePassageiros.length} passageiros.`);
+            console.log(`O passageiro não pode entrar no elevador. Agora o elevador tem ${this.listaDePassageiros.length} passageiros.`);
 
 
-        }
+        };
 
-        ;
+
     };
     // 4°
     //4.1
     movimentar(andarDeDestino) {
         this.andarAtual = andarDeDestino;
-        console.log(this.andarAtual);
-    }
+        console.log(`Agora o elevador está no andar ${this.andarAtual}.`);
 
-    //4.2    
-    removerPassageiros(passageiro) {
+        let contar = 0;
+
         for (let i = 0; i < this.listaDePassageiros.length; i++) {
             if (this.listaDePassageiros[i].andarPretendido == this.andarAtual) {
-
+                this.listaDePassageiros.splice(i, 1);
+                contar++
             }
-        }
+        };
 
-    }
-};
+        console.log(`${contar} saíram do elevador.`);
+        console.log(`${this.listaDePassageiros.length} continuam no elevador.`);
+    };
+}
+
 
 

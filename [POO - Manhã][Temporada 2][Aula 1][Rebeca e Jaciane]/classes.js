e47ade191231d5b4c9e07924474c07cce00cc9f5
Caetano <130772629+rsc-vi@users.noreply.github.com>
2023-08-08 10:43:29 -0300

    Add files via upload

---
+++ [POO - Manhã][Temporada 2][Aula 1][Rebeca e Jaciane]/classes.js
@@ -0,0 +1,12 @@
+//crie uma classe Aluno
+//ele devera ter 3 atributos
+//nome
+//idade
+//matricula
+class Aluno{
+    constructor(nome, idade, matricula){
+        this.nome = nome;
+        this.idade = idade;
+        this.matricula = matricula;
+    }
+}

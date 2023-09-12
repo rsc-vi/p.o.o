3abfa10f0c6d8b59088a868fd82fda1d052759f6
Caetano <130772629+rsc-vi@users.noreply.github.com>
2023-09-12 09:09:37 -0300

    Update classes.js

--- 2° temporada /aula 2/classes.js
+++ 2° temporada /aula 2/classes.js
@@ -3,11 +3,29 @@
 //nome
 //idade
 //matricula
+// quadradinho do css >> display flex
 class Aluno{
-    constructor(nome, idade, matricula, foto){
-        this.nome = nome;
-        this.idade = idade;
+    ///construtor gera o objeto
+    constructor(turma, primeironome, ultimonome, dataDeNascimento, matricula, foto){
+        this.turma = turma;
+        this.primeironome = primeironome;
+        this.ultimonome = ultimonome;
+        
+        this.dataDeNascimento = dataDeNascimento;
         this.matricula = matricula;
-        this.foto = foto
+        this.foto = foto;
+    }
+    getNome(){
+        return`${this.ultimonome} , ${this.primeironome}`
+    }
+
+    getIdade(){
+        let hoje = new Date(); 
+        return hoje.getFullYear() - this.dataDeNascimento.getFullYear();
+
+
+        //return( hoje.getFullYear() - this.dataDeNascimento.getFullYear() );
     }
 }
+
+

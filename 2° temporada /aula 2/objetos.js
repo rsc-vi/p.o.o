869a207a22640f81d1d0ab442c6186f1c02370e6
Caetano <130772629+rsc-vi@users.noreply.github.com>
2023-09-12 10:46:31 -0300

    Update objetos.js
    aula 12/09

--- 2° temporada /aula 2/objetos.js
+++ 2° temporada /aula 2/objetos.js
@@ -1,53 +1,22 @@
-let aluno1 = new Aluno('912-A', 'Rebeca', 16, 20232006, 'https://lh3.googleusercontent.com/d/1vw8sfjAXTlZpfwTJNJSXJQd1sOgKiVb0')
-let aluno2 = new Aluno('912-A', 'Jaciane', 16, 20232007, 'https://lh3.googleusercontent.com/d/1d5oA9rmApqBH6Mt3n75Dto7bETmuus6x');
-let aluno3 = new Aluno('912-A', 'Thayslane', 17, 20232008, 'https://lh3.googleusercontent.com/d/1kQcfoPxzC3k0Jje8t6Qwo8we7JQhsdvR');
-//console.log(aluno1);
+let aluno01 = new Aluno('912-A', 'Rebeca ', " Mabel", new Date("2006-9-7"), 20232006, 'https://lh3.googleusercontent.com/d/1vw8sfjAXTlZpfwTJNJSXJQd1sOgKiVb0');
+let aluno02 = new Aluno('912-A', 'Jaciane ', " Bee", new Date("2007-3-3"), 20232007, 'https://lh3.googleusercontent.com/d/1d5oA9rmApqBH6Mt3n75Dto7bETmuus6x');
+let aluno03 = new Aluno('912-A', 'Thayslane ', " Machado", new Date("2006-7-10"), 20232008, 'https://lh3.googleusercontent.com/d/1kQcfoPxzC3k0Jje8t6Qwo8we7JQhsdvR');
+let aluno04 = new Aluno('912-A', 'giiiiiiii ', " Ferreira", new Date("2006-9-3"), 20232009, 'https://drive.google.com/file/d/1D0tK367ziUWFUs2PgIVZKbTHEuiPV43r')
+let aluno05 = new Aluno('912-A', 'Ellen ', " Petrolina", new Date("2006-12-5"), 20232010, 'https://drive.google.com/file/d/1xkGio1rSh25XTFnKAHcwXXsFliwU22HQ');
+let aluno06 = new Aluno('912-A', 'Leticia ', " Hogwarts", new Date("2006-1-18"), 20232011, 'https://drive.google.com/file/d/11_YLnG8WyA_1zeUaUp0yfUFUBoCsdoz8');
+let aluno07 = new Aluno('912-A', 'Ruby ', " Potter", new Date("2007-8-28"), 20232017, 'https://drive.google.com/file/d/1mGY2Y1oyIks5f7OvgCtd5lEysC5oe1uX')
+let aluno08 = new Aluno('912-A', 'Davi ', " Lima", new Date("2006-4-13"), 20232013, 'https://drive.google.com/file/d/1bMGWpQ8K7ZMRXxjZldXs0DFlLylREE6I');
+let aluno09 = new Aluno('912-A', 'Thiago ', " Rood", new Date("2007-2-12"), 20232014, 'https://drive.google.com/file/d/1yK1BWGSRxuHt4gP6mmYNOIRHs4plJ8gR');
+let aluno10 = new Aluno('912-A', 'Catarina ', " Bob", new Date("2007-5-29"), 20232015, 'https://drive.google.com/file/d/1tCJ1hVCp7445_lj-Dj4LZ0yOaTAfxwA2');
+
+console.log(aluno01.getIdade());
+
 //crie uma array, depois adicione os tês alunosa esse array
 let listaDeAlunos = [];
-listaDeAlunos.push(aluno1, aluno2, aluno3)
+listaDeAlunos.push(aluno01, aluno02, aluno03, aluno04, aluno05, aluno06, aluno07, aluno08, aluno09, aluno10)
 
 //percorra o array e imprima os dados de cada
 //aluno no console
-for (let i = 0; i < listaDeAlunos.length; i++) {
-
-    document.write(`  
-   <div class='card'>
-        
-        <div class='cabecalho'>
-            <div>
-                <p class='turma'>${listaDeAlunos[i].turma}</p>
-                <h3 class='nome'>${listaDeAlunos[i].nome}</h3>
-            </div>
-            <img class='logo' src='img/IFAL.png'/>
-        </div>
 
 
 
-        </div>
-    
-     <div class='conteudo'>
-        <img class='foto' src='${listaDeAlunos[i].foto.replace('https://drive.google.com/file', 'https://lh3.googleusercontent.com')}'>
-    <div class='info'>
-    <div>
-        <p class='label'>Idade</p>
-        <p class='valor'>${listaDeAlunos[i].idade}</p>
-       
-    </div>
-       
-    <div>
-            <p class='label'>Matricula</p>
-            <p class='valor'>${listaDeAlunos[i].matricula}</p>
-        </div>
-    </div>
-    
-   <div>Turma:${listaDeAlunos[i].turma}</div>
-   <div>Nome:${listaDeAlunos[i].nome}</div>
-   <div>Idade:${listaDeAlunos[i].idade}</div>
-   <div>Matricula:${listaDeAlunos[i].matricula}</div>
-
-    </div>`)
-}
-
-for (let i = 0; i < listaDeAlunos.length; i++) {
-    console.table(listaDeAlunos[i])
-}

4a2dba57186715956b73727621af67b409a384c7
Caetano <130772629+rsc-vi@users.noreply.github.com>
2023-10-09 09:17:36 -0300

    Update classes.js

--- 2° temporada /animais/classes.js
+++ 2° temporada /animais/classes.js
@@ -1,1 +1,44 @@
+class Animal {
+    constructor(nome) {
+        this.nome = nome;
+    }
+
+    emitirSom() {
+        console.log('RAWH');
+    }
+}
+// -----------------------------------------------------
+class Cachorro extends Animal {
+    constructor(nome) {
+        super(nome);
+    }
+
+    emitirSom() {
+        console.log('AUAU');
+    }
+}
+// -----------------------------------------------------
+class Gato extends Animal {
+    constructor(nome) {
+        super(nome);
+    }
+
+    emitirSom() {
+        console.log('MIAU MIAU, MEU PARCEIRO');
+    }
+}
+// -----------------------------------------------------
+class Siames extends Gato {
+    // Quando há variação nas entradas de de um método,
+    // diz-se que há uma SOBRECARGA do método. 
+    constructor(nome, cor='cinza') {
+        super(nome);
+        this.cor = cor;
+    }
+
+    emitirSom() {
+        console.log('MIAU MIAU, MR. XADEN');
+    }
+}
+// -----------------------------------------------------
 

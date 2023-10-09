a5b8a0b3abda0d046219b1cdee2cecc7be9cea78
Caetano <130772629+rsc-vi@users.noreply.github.com>
2023-10-09 09:18:17 -0300

    Add files via upload

---
+++ 2° temporada /animais/teste.js
@@ -0,0 +1,9 @@
+let gatoSevergonho = new Gato('Floquinho');
+let cachorroSafado = new Cachorro('Satanás');
+let gatoSiames = new Siames('Violet', 'Prateada');
+
+// -----------------------------------
+gatoSevergonho.emitirSom();
+cachorroSafado.emitirSom();
+gatoSiames.emitirSom();
+console.log(gatoSiames);
